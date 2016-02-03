afacd3f966fd1bf9d5aebaafb4b01aa95d3a4a18
Jimmy Cook <jimmy.cook.1993@gmail.com>
2016-02-03 15:03:25 +0000

    added meta viewport

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,12 @@
 <!doctype html>
 <html>
     <head>
-        <title>@yield('title')</title>        
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>@yield('title')</title>        
+
         <link href="/css/app.css" rel="stylesheet">   
          
     </head>
