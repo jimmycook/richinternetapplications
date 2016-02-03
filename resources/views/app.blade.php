345a18e712196281a1d50ac2b87373cad8a9f545
Jimmy Cook <jimmy.cook.1993@gmail.com>
2016-02-03 15:07:53 +0000

    trying to fix on mobile

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
-        <title>@yield('title')</title>        
+        <title>@yield('title') Rich Internet Applications</title>        
 
         <link href="/css/app.css" rel="stylesheet">   
          
