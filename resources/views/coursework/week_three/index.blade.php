c43009048519187b9f8afe49a911240d14022d22
Jimmy <jimmy.cook.1993@gmail.com>
2016-03-01 17:20:30 +0000

    sidebar week 3 finished

--- resources/views/coursework/week_three/index.blade.php
+++ resources/views/coursework/week_three/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="container">
     <div class="Menu__container">
-        Navbar
+        @include('coursework.week_three.navbar')
     </div>
     <div class="Menu__container">
         @include('coursework.week_three.tabs')
     </div>
     <div class="Menu__container">
-        Sidebar
+        @include('coursework.week_three.sidebar')
     </div>
 </div>
 @endsection
